c72e81d5671ef2a5f4f89d0167d669d67272e839
zikunezhang <zz3367@columbia.edu>
2026-01-01 23:00:05 +0000

    Deploying to gh-pages from @ zikunezhang/zikunezhang.github.io@8360ce516714a701ffb215e904bf62d04f706c8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,17 +41,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/math/";
           
         },
-      },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
       },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
